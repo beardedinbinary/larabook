5eee615a7063f370e3869b5aa7cba7518055f07a
Steve Balistreri <steve@flipeleven.com>
2014-07-28 01:19:08 -0500

    Registration Validation Lesson Complete

--- app/controllers/RegistrationController.php
+++ app/controllers/RegistrationController.php
@@ -1,6 +1,15 @@
 <?php
 
+use Larabook\Forms\RegistrationForm;
+
 class RegistrationController extends \BaseController {
+	
+	private $registrationForm;
+
+	function __construct(RegistrationForm $registrationForm)
+	{
+		$this->registrationForm = $registrationForm;
+	}
 
 	/**
 	 * Show the form for creating a new resource.
@@ -19,6 +28,9 @@ class RegistrationController extends \BaseController {
 	 */
 	public function store()
 	{
+		$this->registrationForm->validate(Input::all());
+
+
 		$user = User::create(
 			Input::only('username', 'email', 'password')
 		);
--- app/views/registration/create.blade.php
+++ app/views/registration/create.blade.php
@@ -2,33 +2,39 @@
 
 @section('content')
 
-<h1>Register!</h1>
-{{ Form::open(['route' => 'register_path']) }}
-<div class="form-group">
-	{{ Form::label('username', 'Username:') }}
-	{{ Form::text('username', null, ['class' => 'form-control']) }}
+<div class="row">
+	<div class="col-md-6">
+		<h1>Register!</h1> 
+		@include('layouts.partials.errors')
+
+		{{ Form::open(['route' => 'register_path']) }}
+		<div class="form-group">
+			{{ Form::label('username', 'Username:') }}
+			{{ Form::text('username', null, ['class' => 'form-control']) }}
+		</div>
+
+		<div class="form-group">
+			{{ Form::label('email', 'Email:') }}
+			{{ Form::text('email', null, ['class' => 'form-control']) }}
+		</div>
+
+
+		<div class="form-group">
+			{{ form::label('password', 'Password:') }}
+			{{ form::password('password', ['class' => 'form-control']) }}
+		</div>
+
+		<div class="form-group">
+			{{ form::label('password_confirmation', 'Password Confirmation:') }}
+			{{ form::password('password_confirmation', ['class' => 'form-control']) }}
+		</div>
+
+		<div class="form-group">
+			{{ Form::submit('Sign Up', ['class' => 'btn btn-primary']) }}
+		</div>
+
+		{{ Form::close() }}
+	</div>
 </div>
-
-<div class="form-group">
-	{{ Form::label('email', 'Email:') }}
-	{{ Form::text('email', null, ['class' => 'form-control']) }}
-</div>
-
-
-<div class="form-group">
-	{{ form::label('password', 'Password:') }}
-	{{ form::password('password', ['class' => 'form-control']) }}
-</div>
-
-<div class="form-group">
-	{{ form::label('password_confirmation', 'Password Confirmation:') }}
-	{{ form::password('password_confirmation', ['class' => 'form-control']) }}
-</div>
-
-<div class="form-group">
-	{{ Form::submit('Sign Up', ['class' => 'btn btn-primary']) }}
-</div>
-
-{{ Form::close() }}
 @stop
 
